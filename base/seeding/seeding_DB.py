6f40418e48e8075443f2f409aafaf0d04d3ce744
Khalid Zakariya Abonjila <85053206+Khaledz148@users.noreply.github.com>
2023-10-14 08:29:45 +0300

    fixed conflicts

--- base/seeding/seeding_DB.py
+++ base/seeding/seeding_DB.py
@@ -1,20 +1,21 @@
 from ..models import Mobileclinic, Activity, Patient, HistoricalActivity, PredictionActivity
 from django.shortcuts import get_list_or_404
+
+
 def seed():
     mobileclinics = get_list_or_404(Mobileclinic)
 
     for mobileclinic in mobileclinics:
         activities = Activity.objects.filter(mobile_clinic=mobileclinic)
-        
-<<<<<<< HEAD
+
         for activity in activities:
             patients = Patient.objects.filter(Activity=activity)
             zone = [activity.latitude, activity.longitude]
-            
+
             for patient in patients:
                 HistoricalActivity.insert_one(
                     {
-                    # we need AVg annual disaster and weather fluctuations
+                        # we need AVg annual disaster and weather fluctuations
                         # from mobile clinic
                         'num_of_staff': mobileclinic.num_of_staff,
                         'clinic_services': mobileclinic.clinic_services,
@@ -36,35 +37,35 @@ def seed():
                 )
     print('seeding..')
 
-        if activities is not None:
-            for activity in activities:
-                patients = Patient.objects.filter(Activity=activity)
-                zone = [activity.latitude, activity.longitude]
-                
-                if patients is not None:
-                    for patient in patients:
-                        HistoricalActivity.insert_one(
-                            {
+    if activities is not None:
+        for activity in activities:
+            patients = Patient.objects.filter(Activity=activity)
+            zone = [activity.latitude, activity.longitude]
+
+            if patients is not None:
+                for patient in patients:
+                    HistoricalActivity.insert_one(
+                        {
                             # we need AVg annual disaster and weather fluctuations
-                                # from mobile clinic
-                                'num_of_staff': mobileclinic.num_of_staff,
-                                'clinic_services': mobileclinic.clinic_services,
-                                'clinic_capacity': mobileclinic.clinic_capacity,
-                                'total_annual_budget': mobileclinic.total_annual_budget,
-                                'pharmaceutical_expenditure': mobileclinic.pharmaceutical_expenditure,
-                                'pharmaceutical_waste': mobileclinic.pharmaceutical_waste,
-                                # from activity
-                                'num_of_patients': activity.num_of_patients,
-                                'zone': zone,
-                                'date': f'{activity.date}',
-                                'population_density': activity.population_density,
-                                'crisis_type': activity.crisis_type,
-                                # from patient
-                                'age': patient.age,
-                                'gender': patient.gender,
-                                'diagnosis': patient.diagnosis,
-                            }
-                        )
+                            # from mobile clinic
+                            'num_of_staff': mobileclinic.num_of_staff,
+                            'clinic_services': mobileclinic.clinic_services,
+                            'clinic_capacity': mobileclinic.clinic_capacity,
+                            'total_annual_budget': mobileclinic.total_annual_budget,
+                            'pharmaceutical_expenditure': mobileclinic.pharmaceutical_expenditure,
+                            'pharmaceutical_waste': mobileclinic.pharmaceutical_waste,
+                            # from activity
+                            'num_of_patients': activity.num_of_patients,
+                            'zone': zone,
+                            'date': f'{activity.date}',
+                            'population_density': activity.population_density,
+                            'crisis_type': activity.crisis_type,
+                            # from patient
+                            'age': patient.age,
+                            'gender': patient.gender,
+                            'diagnosis': patient.diagnosis,
+                        }
+                    )
     print('seeding..')
 
 # the rest
@@ -73,13 +74,14 @@ def seed():
 
 #     for mobileclinic in mobileclinics:
 #         activities = Activity.objects.filter(mobile_clinic=mobileclinic)
-        
+
 #         if activities is not None:
 #             for activity in activities:
 #                 if activity.date == datetime.date.today():
 #                     patients = Patient.objects.filter(Activity=activity)
 #                     zone = [activity.latitude, activity.longitude]
-                    
+
+
 #                     if patients is not None:
 #                         for patient in patients:
 #                             HistoricalActivity.insert_one(
@@ -108,4 +110,4 @@ def seed():
 #  for prediction
 # zone
 # Age
-# diagnosis_percentage 
+# diagnosis_percentage
